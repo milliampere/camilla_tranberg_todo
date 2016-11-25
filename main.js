4479eebd0e8304426cb4e255b5edb393c4bce014
milliampere <millla@hotmail.com>
2016-11-25 10:04:01 +0100

    2.0

--- main.js
+++ main.js
@@ -10,11 +10,22 @@ var saveButton = document.getElementById("saveButton");
 saveButton.addEventListener("click", addTask);
 
 
+// Specialare: Spara när trycker på enter
+var textInput = document.getElementById("textInput");
+textInput.addEventListener("keyup", function(event) {
+	var x = event.keyCode;
+	if(x == 13){
+		console.log("Lägger till uppgift med enterknappen :)");
+		addTask();
+	}
+});	
+
+
 // Uppdatera listans event
 function update() {
 
 	// Lägger till event till checkbox
-	var inputList = document.getElementsByClassName("cbox");
+	var inputList = document.getElementsByClassName("checkbox");
 	for(var i = 0; i < inputList.length; i++){
 	inputList[i].addEventListener("change", taskIsDone);
 	}
@@ -22,9 +33,14 @@ function update() {
 	// Lägger till event till papperskorg
 	var spanList = document.getElementsByTagName("span");
 	for(var j = 0; j < spanList.length; j++){
-		spanList[j].addEventListener("click", removeTask);	
+		spanList[j].addEventListener("click", removeTask);
+		spanList[j].addEventListener("mouseover", function(){
+    		this.style.color = "grey";
+    	});
+    	spanList[j].addEventListener("mouseout", function(){
+    		this.style.color = "black";
+    	});
 	}
-
 }
 
 
@@ -33,6 +49,7 @@ function update() {
 /**************************/
 
 // Funktion: Lägga till syssla i lista
+
 function addTask(){
 	var textInput = document.getElementById("textInput");
 
@@ -41,6 +58,12 @@ function addTask(){
 		alert("Hallå latmask, skriv in en uppgift!");
 	}
 	else {
+
+		// Kapar texten om den är för lång (kanske inte snällt men livet är hårt)
+		if(textInput.value.length > 40){
+			textInput.value = textInput.value.substring(0,40) + "...";
+		}
+
 		var text = document.createTextNode(textInput.value);
 
 		//Hämtar ul-listan
@@ -52,15 +75,15 @@ function addTask(){
 		// Skapar en checkbox
 		var checkbox = document.createElement("input");
 		checkbox.setAttribute("type", "checkbox");
-		checkbox.setAttribute("class", "cbox");
+		checkbox.setAttribute("class", "checkbox");
 
 		// Fäster input till li och li till ul 
 		taskLi.appendChild(checkbox);
 		taskLi.appendChild(text);
 		unfinishedTasks.appendChild(taskLi);
 		
-		// Lägger till en soptunna-ikon
-		taskLi.innerHTML += "<span class='glyphicon glyphicon-trash'></span>";
+		// Lägger till en soptunna
+		taskLi.innerHTML += "<span class='trash'>Radera</span>";
 
 		// Tömmer textfältet
 		textInput.value = "";
@@ -77,17 +100,19 @@ function addTask(){
 // Funktion: Lägga till syssla i avklarat
 
 function taskIsDone(){
-		
+
 	// This är input-elementet, parentElement är li
 	var checkedTask = this.parentElement;
-	console.log("Flyttar " + checkedTask.innerText);
-	// Detacha från ul och lägg i ny ul
-	checkedTask.parentElement.removeChild(checkedTask);
+	console.log("Flyttar ");
+
+	// Lägger till i ny ul
 	var ul = document.getElementById("finishedTasks");
 	ul.appendChild(checkedTask);
+
 	// Ta bort checkbox
 	checkedTask.removeChild(this);
 
+	// Stylar om
 	checkedTask.style.color = "grey";
 	checkedTask.style.textDecoration = "line-through";
 
@@ -101,11 +126,12 @@ function taskIsDone(){
 /**************************/
 
 // Funktion: Ta bort en syssla
+
 function removeTask(){
 
 	// this är span-elementet, parentElement är li
 	var clickedBin = this.parentElement;
-	console.log("Raderar " + clickedBin.innerText);
+	console.log("Raderar ");
 
 	// Tar bort li
 	clickedBin.parentElement.removeChild(clickedBin);
